8429754313d1820f3c4d0b4d4b8381c9db47bd5f
sifeddinemeb <sifeddine.meb96@gmail.com>
2025-06-25 22:05:08 +0100

    Fix duplicate entries in tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -40,18 +40,6 @@ export default {
           800: '#1f2937',
           900: '#111827',
         },
-        gray: {
-          50: '#f9fafb',
-          100: '#f3f4f6',
-          200: '#e5e7eb',
-          300: '#d1d5db',
-          400: '#9ca3af',
-          500: '#6b7280',
-          600: '#4b5563',
-          700: '#374151',
-          800: '#1f2937',
-          900: '#111827',
-        },
       },
       backgroundImage: {
         'grain': "url('data:image/svg+xml,%3Csvg width=\"200\" height=\"200\" viewBox=\"0 0 200 200\" xmlns=\"http://www.w3.org/2000/svg\"%3E%3Cfilter id=\"noiseFilter\"%3E%3CfeTurbulence type=\"fractalNoise\" baseFrequency=\"0.85\" numOctaves=\"4\" stitchTiles=\"stitch\"/%3E%3C/filter%3E%3Crect width=\"100%25\" height=\"100%25\" filter=\"url(%23noiseFilter)\" opacity=\"0.4\"/%3E%3C/svg%3E')",
@@ -113,33 +101,8 @@ export default {
       'xl': '1280px',
       '2xl': '1536px',
     },
-    screens: {
-      'xs': '480px',
-      'sm': '640px',
-      'md': '768px',
-      'lg': '1024px',
-      'xl': '1280px',
-      '2xl': '1536px',
-    },
   },
   plugins: [
-    function({ addBase }) {
-      addBase({
-        'html': { 
-          fontSize: '16px', 
-          overflowX: 'hidden' 
-        },
-        'body': {
-          maxWidth: '100vw',
-          overflowX: 'hidden'
-        },
-        'table': {
-          borderCollapse: 'separate',
-          borderSpacing: 0,
-          width: '100%'
-        }
-      });
-    },
     function({ addBase }) {
       addBase({
         'html': { 
